2f7170dc9fe371d65feac24846272d8ecd7de6b4
Emanuel-Boaventura <emanuelbm.dev@gmail.com>
2024-11-04 19:35:17 -0400

    feat: configure env and base of e2e tests

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('POST/auth/signup - handles a signup request', () => {
+    const testEmail = 'uniqueemail2@email.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: testEmail, password: '123' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(testEmail);
+      });
+  });
+});
